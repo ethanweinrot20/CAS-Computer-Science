d0b90968c80fd99edf8d47d544c5c70b3471f811
Ethan Weinrot <ethan.weinrot@gmail.com>
2019-09-12 11:07:31 -0700

    Functions Assignment

---
+++ Functions Assignment 9:11:19/functions_assignment_9_11_19.playground/Contents.swift
@@ -0,0 +1,31 @@
+//This function squares numbers
+func square(number: Int) -> Int {
+    let squared = number * number
+    return(squared)
+}
+print(square(number: 18))
+
+//This function just returns/prints 5
+func five() -> Int {
+    return 5
+}
+print(five())
+
+//This number takes two strings and prints them together
+func fullName(first: String, last: String) -> String{
+    return first + " " + last
+}
+print(fullName( first: "Ethan", last: "Weinrot"))
+
+//This function takes a string and puts an exclamation point after it
+func shout(input: String) -> String {
+    return input + "!"
+}
+print(shout(input: "YO"))
+
+//This function turns an int into a string.
+func numberName(numbers: Int) -> String {
+    let letters = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    return letters[numbers]
+}
+print(numberName(numbers: 5))
