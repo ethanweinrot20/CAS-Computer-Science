7de5a22212fb883cd96620fcff717e38e5c62946
Ethan Weinrot <ethan.weinrot@gmail.com>
2019-09-27 10:10:32 -0700

    Choose Your Own Adventure Assignment
    I'm not done yet

---
+++ ChooseYourOwnAdventure/ChooseYourOwnAdventure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChooseYourOwnAdventure
+//
+//  Created by Ethan Weinrot on 9/27/19.
+//  Copyright © 2019 Ethan Weinrot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
