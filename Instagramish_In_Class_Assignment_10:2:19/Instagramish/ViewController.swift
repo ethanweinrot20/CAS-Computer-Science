6031e53858ae75610319f155470226bae097f663
Ethan Weinrot <ethan.weinrot@gmail.com>
2019-10-02 09:07:33 -0700

    10/2/19 In Class Assignment

---
+++ Instagramish_In_Class_Assignment_10:2:19/Instagramish/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagramish
+//
+//  Created by Ethan Weinrot on 10/2/19.
+//  Copyright © 2019 Ethan Weinrot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
